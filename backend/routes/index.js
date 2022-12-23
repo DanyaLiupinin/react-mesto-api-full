deaf645e0506e1f1753b69e1df456afd31849192
Danya Kovan <mr.kovan_2002@mail.ru>
2022-12-23 15:01:16 +0100

    -crash-test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,12 +6,6 @@ const { login, createUser } = require('../controllers/users');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),

d5f4ba5b4cd6b98c24b44e834ebf26a3241409cd
Danya Kovan <mr.kovan_2002@mail.ru>
2023-05-15 13:15:39 +0400

    -

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,9 @@ require('dotenv').config();
 
 const app = express();
 const bodyParser = require('body-parser');
-// const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const cors = require('cors');
-// const userRouter = require('./routes/users');
-// const cardRouter = require('./routes/cards');
-// const { login, createUser } = require('./controllers/users');
-// const { auth } = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
-// const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
 
@@ -37,30 +31,6 @@ app.listen(3000);
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.use(router);
-
-/*
-app.use('/users', auth, userRouter);
-app.use('/cards', auth, cardRouter);
-*/
-
-/*
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/https?:\/\/(www\.)?\d?\D{1,}#?/),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-*/
 
 app.use(errorLogger);
 
